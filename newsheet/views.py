4d3e813a99137a82d4f8cdbd679fa625e225a475
Morgan <glalum603@gmail.com>
2024-09-24 15:44:42 -0500

    Added "Add audio" button

--- newsheet/views.py
+++ newsheet/views.py
@@ -26,32 +26,15 @@ class NewSheetView(CTkFrame):  # Inheriting from CTkFrame instead of CTk
         self.LB_COMPOSITIONS = CTkLabel(master=self.OPTIONSFRAME, text="File", height=25, text_color=("#ffffff", "#ffffff"), font=CTkFont(size=15, family="Courier New", weight="bold"))
         self.LB_COMPOSITIONS.pack(padx=(15, 0), pady=(10, 0), anchor="w")
 
-        self.BTN_SAVE = CTkButton(master=self.OPTIONSFRAME, text="Save", text_color=("#ffffff", "#ffffff"), anchor="w", width=180, image=CTkImage(Image.open(r"assets/Assets/baseline_save_(255, 255, 255)_18dp_1x.png"), size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
-        self.BTN_SAVE.pack(padx=(10, 0), pady=(10, 0), anchor="w")
-
-        self.BTN_DELETE = CTkButton(master=self.OPTIONSFRAME, text="Delete", text_color=("#ffffff", "#ffffff"), anchor="w", width=180, image=CTkImage(Image.open(r"assets/Assets/baseline_delete_(255, 255, 255)_18dp_1x.png"), size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
-        self.BTN_DELETE.pack(padx=(10, 0), pady=(10, 0), anchor="w")
-
-        self.BTN_NEW = CTkButton(master=self.OPTIONSFRAME, text="New", text_color=("#ffffff", "#ffffff"), anchor="w", width=180, image=CTkImage(Image.open(r"assets/Assets/baseline_plus_(255, 255, 255)_18dp_1x.png"), size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
-        self.BTN_NEW.pack(padx=(10, 0), pady=(10, 0), anchor="w")
+        self.BTN_ADD = CTkButton(master=self.OPTIONSFRAME, text="Add Audio", text_color=("#ffffff", "#ffffff"), 
+                                  anchor="w", width=180, 
+                                  image=CTkImage(Image.open(r"assets/Assets/baseline_save_(255, 255, 255)_18dp_1x.png"), 
+                                  size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), 
+                                  hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
+        self.BTN_ADD.pack(padx=(10, 0), pady=(10, 0), anchor="w")
 
         self.LB_HZLINE = CTkLabel(master=self.OPTIONSFRAME, text="-----------------------------------------------------------------------------------", anchor="s", justify="center", height=0, text_color=("gray14", "#808080"), font=CTkFont(size=9))
         self.LB_HZLINE.pack(pady=(10, 0))
-
-        self.LB_SOURCE = CTkLabel(master=self.OPTIONSFRAME, text="Audio Source", height=25, text_color=("#ffffff", "#ffffff"), font=CTkFont(size=15, family="Courier New", weight="bold"))
-        self.LB_SOURCE.pack(padx=(15, 0), pady=(20, 0), anchor="w")
-
-        self.BTN_CAS = CTkButton(master=self.OPTIONSFRAME, text="Input Source", text_color=("#ffffff", "#ffffff"), anchor="w", width=180, image=CTkImage(Image.open(r"assets/Assets/baseline_source_settings_(255, 255, 255)_18dp_1x.png"), size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
-        self.BTN_CAS.pack(padx=(10, 0), pady=(10, 0), anchor="w")
-
-        self.LB_HZLINE = CTkLabel(master=self.OPTIONSFRAME, text="-----------------------------------------------------------------------------------", anchor="s", justify="center", height=0, text_color=("gray14", "#808080"), font=CTkFont(size=9))
-        self.LB_HZLINE.pack(pady=(10, 0))
-
-        self.LB_COMPOSITIONS = CTkLabel(master=self.OPTIONSFRAME, text="Export", height=25, text_color=("#ffffff", "#ffffff"), font=CTkFont(size=15, family="Courier New", weight="bold"))
-        self.LB_COMPOSITIONS.pack(padx=(15, 0), pady=(20, 0), anchor="w")
-
-        self.BTN_CONVERT = CTkButton(master=self.OPTIONSFRAME, text="Create Score", text_color=("#ffffff", "#ffffff"), anchor="w", width=180, image=CTkImage(Image.open(r"assets/Assets/baseline_open_new_(255, 255, 255)_18dp_1x.png"), size=(18, 18)), fg_color=("#212121", "gray13"), bg_color=("#212121", "gray13"), hover_color=("#ff8080", "#ff5e5e"), font=CTkFont(size=15, family="Courier New"))
-        self.BTN_CONVERT.pack(padx=(10, 0), pady=(10, 0), anchor="w")
 
         # Main frame
         self.MAINFRAME = CTkFrame(master=self, width=590, height=514, bg_color=("gray92", "#3e3e3e"), fg_color=("gray90", "#3e3e3e"))
