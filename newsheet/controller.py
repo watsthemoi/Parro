4c4fcbbce1597ce38f02562ab866fe8422e6d242
Morgan <glalum603@gmail.com>
2024-10-09 05:07:25 -0500

    Restored predict() changes

--- newsheet/controller.py
+++ newsheet/controller.py
@@ -1,10 +1,13 @@
 import customtkinter as ctk
 from tkinter import filedialog, messagebox
 from newsheet.models import NewSheetModel
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
 
 class NewSheetController:
     def __init__(self, root):
         self.models = NewSheetModel()
+        self.note_events = []
 
     def add_audio_file(self):
         # Open file dialog to select an audio file
@@ -15,12 +18,19 @@ class NewSheetController:
 
         if file:
             try:
-            # Store the file in the model
-                self.models.store_file(file)
-                return file
+                # Store the file in the model
+                self.models.store_infile(file)
+                return self.process_file(file)
             except Exception as e:
-                print("File Access Error: ", e)   
+                messagebox.showerror("Error", f"File Access Error: {e}")
         return None
             
-            
-            
+    def process_file(self, file):
+        """Processes file for pitch data with predict()"""
+        try:
+            model_output, midi_data, note_events = predict(file)
+            self.note_events.extend(note_events)  # Store note events
+            self.models.store_note_events(note_events)    
+            return note_events
+        except Exception as e:
+            raise e       
--- newsheet/models.py
+++ newsheet/models.py
@@ -1,15 +1,26 @@
-
-
 class NewSheetModel:
     def __init__(self):
-        self.audio_file = None
+        self.audio_infile = None
+        self.audio_outfile = None
+        self.note_events = []
 
-    def store_file(self, file):
+    def store_infile(self, file):
         """Store the selected file's path in the model."""
-        self.audio_file = file
+        self.audio_infile = file
 
-    def get_file(self):
-        """Retrieve the stored file's path."""
-        return self.audio_file
+    def store_outfile(self, file):
+        """Store the resulting MIDI of the processing"""
+        self.audio_outfile = file
 
+    def store_note_events(self, note_events):
+        """Store note events"""
+        self.note_events = note_events
 
+    def get_infile(self):
+        """Retrieve the stored input file"""
+        return self.audio_infile
+    
+    def get_outfile(self):
+        """Retrieve the resulting MIDI file"""
+        return self.audio_outfile
+    
